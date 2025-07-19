60e75d4f78f6d9d7bccc1795d7f14970bd6d65cc
obsidyne <obsidyne@gmail.com>
2025-07-19 20:05:51 +0530

    updated pta

--- src/app/about-us/pta/page.jsx
+++ src/app/about-us/pta/page.jsx
@@ -25,17 +25,32 @@ export default function Page() {
                     <tr className='table-row'>
                         <td className='table-data'>PTA President</td>
                         <td className='table-data'>Dr. K. Krishnakumar (Principal)</td>
-                        <td className='table-data'>7907697944</td>
+                        <td className='table-data'>+91 8590332548</td>
                     </tr>
                     <tr className='table-row'>
-                        <td className='table-data'>Parent Representative</td>
-                        <td className='table-data'>Mr. P Salimkumar</td>
-                        <td className='table-data'>9447504384</td>
+                        <td className='table-data'>PTA Vice President</td>
+                        <td className='table-data'>Mr. Jayakumar R (Parent of Sreeparvathy, S5 ECE)</td>
+                        <td className='table-data'>9495478005</td>
                     </tr>
                     <tr className='table-row'>
                         <td className='table-data'>PTA Secretary</td>
-                        <td className='table-data'>Ms. Reeba R (AP, Department of CSE)</td>
-                        <td className='table-data'>9446191031</td>
+                        <td className='table-data'>Mrs. T R Sangeeta (AP, Department of ECE)</td>
+                        <td className='table-data'>-</td>
+                    </tr>
+                    <tr className='table-row'>
+                        <td className='table-data'>PTA Joint Secretary</td>
+                        <td className='table-data'>Mrs. Sheeja E Yohannan (Parent of Neha Siju, S5 CSE)</td>
+                        <td className='table-data'>9495726027</td>
+                    </tr>
+                    <tr className='table-row'>
+                        <td className='table-data'>PTA Treasurer</td>
+                        <td className='table-data'>Ms. Pooja S Mohan (AP, Department of ECE)</td>
+                        <td className='table-data'>9446394252</td>
+                    </tr>
+                    <tr className='table-row'>
+                        <td className='table-data'>PTA Member</td>
+                        <td className='table-data'>Dr. Saji Varghese (Vice Principal, SBCE Pattoor)</td>
+                        <td className='table-data'>-</td>
                     </tr>
                 </tbody>
             </table>
@@ -50,13 +65,14 @@ export default function Page() {
                     </tr>
                 </thead>
                 <tbody>
-                    <tr className='table-row'><td className='table-data'>BT/BCE</td><td className='table-data'>Dr. Anoop Raj J R</td><td className='table-data'>8281868205</td></tr>
-                    <tr className='table-row'><td className='table-data'>CS</td><td className='table-data'>Dr. S V Annlin Jeba</td><td className='table-data'>9487274466</td></tr>
-                    <tr className='table-row'><td className='table-data'>ME</td><td className='table-data'>Dr. Saji Varghese</td><td className='table-data'>8606398371</td></tr>
+                    <tr className='table-row'><td className='table-data'>BT&BCE</td><td className='table-data'>Prof. Meera Bai S</td><td className='table-data'>9446589673</td></tr>
+                    <tr className='table-row'><td className='table-data'>ME</td><td className='table-data'>Prof. Anil Kumar A V</td><td className='table-data'>9947758310</td></tr>
                     <tr className='table-row'><td className='table-data'>CE</td><td className='table-data'>Dr. Gouri Antherjanam</td><td className='table-data'>9446112617</td></tr>
-                    <tr className='table-row'><td className='table-data'>EEE</td><td className='table-data'>Prof. Vinod V P</td><td className='table-data'>9048260779</td></tr>
-                    <tr className='table-row'><td className='table-data'>ECE</td><td className='table-data'>Ms. Pavitha P P</td><td className='table-data'>9995832228</td></tr>
-                    <tr className='table-row'><td className='table-data'>BS</td><td className='table-data'>Prof. Vijayan K</td><td className='table-data'>9447857591</td></tr>
+                    <tr className='table-row'><td className='table-data'>CS</td><td className='table-data'>Dr. S V Annlin Jeba</td><td className='table-data'>9487274466</td></tr>
+                    <tr className='table-row'><td className='table-data'>AI</td><td className='table-data'>Dr. Anil A R</td><td className='table-data'>9447477577</td></tr>
+                    <tr className='table-row'><td className='table-data'>EEE</td><td className='table-data'>Dr. Vinod V P</td><td className='table-data'>9048260779</td></tr>
+                    <tr className='table-row'><td className='table-data'>ECE</td><td className='table-data'>Prof. Pavitha P P</td><td className='table-data'>8075376012</td></tr>
+                    <tr className='table-row'><td className='table-data'>HRD</td><td className='table-data'>Prof. Pradeep Kumar R</td><td className='table-data'>9446472562</td></tr>
                 </tbody>
             </table>
 
@@ -70,12 +86,13 @@ export default function Page() {
                     </tr>
                 </thead>
                 <tbody>
-                    <tr className='table-row'><td className='table-data'>BT/BCE</td><td className='table-data'>Ms. Anju Raj</td><td className='table-data'>9539360681</td></tr>
-                    <tr className='table-row'><td className='table-data'>CS</td><td className='table-data'>Ms. Reeba R</td><td className='table-data'>9446191031</td></tr>
-                    <tr className='table-row'><td className='table-data'>ME</td><td className='table-data'>Mr. Gokul O</td><td className='table-data'>9447988318</td></tr>
+                    <tr className='table-row'><td className='table-data'>BT&BCE</td><td className='table-data'>Dr. Shamnamol</td><td className='table-data'>9539360681</td></tr>
+                    <tr className='table-row'><td className='table-data'>ME</td><td className='table-data'>Mr. Prashanth V</td><td className='table-data'>9496877660</td></tr>
                     <tr className='table-row'><td className='table-data'>CE</td><td className='table-data'>Mr. Ashok Mathew</td><td className='table-data'>9645287537</td></tr>
-                    <tr className='table-row'><td className='table-data'>EEE</td><td className='table-data'>Ms. Juna John Daniel</td><td className='table-data'>9895610435</td></tr>
-                    <tr className='table-row'><td className='table-data'>ECE</td><td className='table-data'>Mr. Vishnu V.S</td><td className='table-data'>9947062806</td></tr>
+                    <tr className='table-row'><td className='table-data'>CS</td><td className='table-data'>Ms. Dhanya S</td><td className='table-data'>9656800583</td></tr>
+                    <tr className='table-row'><td className='table-data'>EEE</td><td className='table-data'>Ms. Sindhuja</td><td className='table-data'>9544112161</td></tr>
+                    <tr className='table-row'><td className='table-data'>ECE</td><td className='table-data'>Ms. Pooja S Mohan</td><td className='table-data'>9446394252</td></tr>
+                    <tr className='table-row'><td className='table-data'>BS</td><td className='table-data'>Ms. Prabhiya P S</td><td className='table-data'>9400154655</td></tr>
                 </tbody>
             </table>
 
@@ -91,21 +108,16 @@ export default function Page() {
                     </tr>
                 </thead>
                 <tbody>
-                    <tr className='table-row'><td className='table-data'>IV</td><td className='table-data'>EEE</td><td className='table-data'>Mr. V K Sreekumar</td><td className='table-data'>Arjun S Kumar</td><td className='table-data'>9447272528</td></tr>
-                    <tr className='table-row'><td className='table-data'>IV</td><td className='table-data'>CS</td><td className='table-data'>Mr. Nazar Kunju</td><td className='table-data'>Bilal</td><td className='table-data'>9446191246</td></tr>
-                    <tr className='table-row'><td className='table-data'>IV</td><td className='table-data'>CE</td><td className='table-data'>Ms. Suja Thomas</td><td className='table-data'>Denna Grace Thomas</td><td className='table-data'>9961855781</td></tr>
-                    <tr className='table-row'><td className='table-data'>III</td><td className='table-data'>EEE</td><td className='table-data'>Ms. Chinmaya Prasad</td><td className='table-data'>Arya C</td><td className='table-data'>9747540816</td></tr>
-                    <tr className='table-row'><td className='table-data'>III</td><td className='table-data'>CS</td><td className='table-data'>Mr. Madhusoodhanan T S</td><td className='table-data'>Meghna Madhu</td><td className='table-data'>9496231359</td></tr>
-                    <tr className='table-row'><td className='table-data'>III</td><td className='table-data'>CE</td><td className='table-data'>Mr. P Salim Kumar</td><td className='table-data'>Jagath Preeth S</td><td className='table-data'>9447504384</td></tr>
-                    <tr className='table-row'><td className='table-data'>III</td><td className='table-data'>BT</td><td className='table-data'>Mr. T Jayamohan</td><td className='table-data'>Abhay J Mohan</td><td className='table-data'>9497487776</td></tr>
-                    <tr className='table-row'><td className='table-data'>III</td><td className='table-data'>ECE</td><td className='table-data'>Mr. Johnson K</td><td className='table-data'>Justy John</td><td className='table-data'>9633752235</td></tr>
-                    <tr className='table-row'><td className='table-data'>III</td><td className='table-data'>ME</td><td className='table-data'>Mr. M S Unnithan</td><td className='table-data'>Kiran Raj</td><td className='table-data'>9496316287</td></tr>
-                    <tr className='table-row'><td className='table-data'>II</td><td className='table-data'>EEE</td><td className='table-data'>Ms. Jeena Jacob</td><td className='table-data'>Joby Sara Jacob</td><td className='table-data'>9447404029</td></tr>
-                    <tr className='table-row'><td className='table-data'>II</td><td className='table-data'>CS</td><td className='table-data'>Ms. Sunitha S</td><td className='table-data'>Megha P S</td><td className='table-data'>9496694208</td></tr>
-                    <tr className='table-row'><td className='table-data'>II</td><td className='table-data'>CE</td><td className='table-data'>Mr. Radha Krishnan</td><td className='table-data'>Harisankar R</td><td className='table-data'>9497259215</td></tr>
-                    <tr className='table-row'><td className='table-data'>II</td><td className='table-data'>BT</td><td className='table-data'>Mr. Mohanlal Raghavan</td><td className='table-data'>Gayathri Mohanlal</td><td className='table-data'>8078268617</td></tr>
-                    <tr className='table-row'><td className='table-data'>II</td><td className='table-data'>ECE</td><td className='table-data'>Mr. Ajith Kumar S</td><td className='table-data'>Athira S Ajith</td><td className='table-data'>8547430674</td></tr>
-                    <tr className='table-row'><td className='table-data'>II</td><td className='table-data'>ME</td><td className='table-data'>Mr. Praveen Kumar</td><td className='table-data'>Akshayn R Praveen</td><td className='table-data'>9447409186</td></tr>
+                    <tr className='table-row'><td className='table-data'>III</td><td className='table-data'>CSE/AI</td><td className='table-data'>Mrs. Sheeja E Yohannan</td><td className='table-data'>Neha Siju</td><td className='table-data'>9495726027</td></tr>
+                    <tr className='table-row'><td className='table-data'>III</td><td className='table-data'>ME</td><td className='table-data'>Mr. Lanil Kumar</td><td className='table-data'>Nanda Kishor A</td><td className='table-data'>9495118331</td></tr>
+                    <tr className='table-row'><td className='table-data'>III</td><td className='table-data'>ECE</td><td className='table-data'>Mr. Jayakumar R</td><td className='table-data'>Sreeparvathy</td><td className='table-data'>9495478005</td></tr>
+                    <tr className='table-row'><td className='table-data'>III</td><td className='table-data'>EEE</td><td className='table-data'>Mr. Anoop P K</td><td className='table-data'>Sandra Anoop</td><td className='table-data'>9446547271</td></tr>
+                    <tr className='table-row'><td className='table-data'>II</td><td className='table-data'>BT/FT</td><td className='table-data'>Mrs. Sreelekha / Mr. Madhu S</td><td className='table-data'>Avani / Manasi</td><td className='table-data'>9539744827 / 9847493229</td></tr>
+                    <tr className='table-row'><td className='table-data'>IV</td><td className='table-data'>CE</td><td className='table-data'>Mr. Pradeep C</td><td className='table-data'>Prince P</td><td className='table-data'>9349472018</td></tr>
+                    <tr className='table-row'><td className='table-data'>I</td><td className='table-data'>ECE/ES</td><td className='table-data'>Mrs. Ambily R</td><td className='table-data'>Amitha/Amrutha</td><td className='table-data'>9847103375</td></tr>
+                    <tr className='table-row'><td className='table-data'>I</td><td className='table-data'>EEE</td><td className='table-data'>Mrs. Bindhu R</td><td className='table-data'>Abhirami B</td><td className='table-data'>7012620587</td></tr>
+                    <tr className='table-row'><td className='table-data'>I</td><td className='table-data'>BT</td><td className='table-data'>Mr. Harikrishnan Nair</td><td className='table-data'>Geethika Harikrishnan</td><td className='table-data'>9745268161</td></tr>
+                    <tr className='table-row'><td className='table-data'>I</td><td className='table-data'>CE</td><td className='table-data'>Dr. Madhu A K</td><td className='table-data'>Giridhar Madhu</td><td className='table-data'>9447484345</td></tr>
                 </tbody>
             </table>
 
