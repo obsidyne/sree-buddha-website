e01f5d2dc27d0e1ded579d3c2962466ab044ffd6
ashvin <obsidyne@gmail.com>
2025-10-06 08:28:33 +0530

    board of studies modified

--- src/app/autonomous/board_of_studies/page.jsx
+++ src/app/autonomous/board_of_studies/page.jsx
@@ -10,22 +10,22 @@ export default function Page() {
     const fetchBoardOfStudies = async () => {
       try {
         // Fetch Board_of_studies
-        const res1 = await fetch(
-          "http://91.99.112.1:1337/api/autonomous?populate[Board_of_studies][populate]=PDF"
-        )
-        const data1 = await res1.json()
-        if (data1?.data?.Board_of_studies) {
-          setBoardDocs(data1.data.Board_of_studies)
-        }
+const res1 = await fetch(
+  `${process.env.NEXT_PUBLIC_STRAPI}/api/autonomous?populate[Board_of_studies][populate]=PDF`
+)
+const data1 = await res1.json()
+if (data1?.data?.Board_of_studies) {
+  setBoardDocs(data1.data.Board_of_studies)
+}
 
         // Fetch board_of_studies_first_meeting
-        const res2 = await fetch(
-          "http://91.99.112.1:1337/api/autonomous?populate[board_of_studies_first_meeting][populate]=file"
-        )
-        const data2 = await res2.json()
-        if (data2?.data?.board_of_studies_first_meeting) {
-          setFirstMeetingDocs(data2.data.board_of_studies_first_meeting)
-        }
+       const res2 = await fetch(
+  `${process.env.NEXT_PUBLIC_STRAPI}/api/autonomous?populate[board_of_studies_first_meeting][populate]=file`
+)
+const data2 = await res2.json()
+if (data2?.data?.board_of_studies_first_meeting) {
+  setFirstMeetingDocs(data2.data.board_of_studies_first_meeting)
+}
       } catch (error) {
         console.error("Error fetching Board of Studies documents:", error)
       }
